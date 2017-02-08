f805c5ed6083e507f91db68bbd306caaf4f32b81
Andrew Morris <voltrevo@gmail.com>
2017-02-08 19:45:59 +1100

    Add driver program

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+'use strict';
 
+const moment = require('moment');
+
+const TrafficIntersection = require('./TrafficIntersection');
+
+const intersection = TrafficIntersection();
+const timestamp = () => moment().format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+
+intersection.events.on('color-change', ({ lightName, newColor }) => {
+  console.log(`${timestamp()}: ${lightName} change to ${newColor}`);
+});
+
+const changeLoop = () => {
+  console.log(`${timestamp()}: change!`);
+  intersection.change();
+
+  setTimeout(changeLoop, Math.random() * 10000);
+};
+
+setTimeout(changeLoop, 3000);
